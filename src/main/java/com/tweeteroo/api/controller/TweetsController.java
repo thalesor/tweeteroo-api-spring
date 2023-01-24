5f4dbaf1818cd94c0c371c111dfb315bae467450
Thales Ruano <thalesoruano@gmail.com>
2023-01-24 13:42:29 -0300

    create /tweets/username GET route

--- src/main/java/com/tweeteroo/api/controller/TweetsController.java
+++ src/main/java/com/tweeteroo/api/controller/TweetsController.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -35,6 +37,25 @@ public class TweetsController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(tweetsMap);
+
+    }
+
+    @GetMapping(value = "/{username}")
+    public ResponseEntity<List<Tweet>> getTweetsFromUserName(
+            @PathVariable("username") String name) {
+
+        List<Tweet> filteredList;
+        List<Tweet> originalList = tweetList;
+
+        filteredList = originalList.stream()
+                .filter(tweet -> tweet.getUserName().equals(name))
+                .collect(Collectors.toList());
+
+        Collections.reverse(filteredList);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(filteredList);
 
     }
 
