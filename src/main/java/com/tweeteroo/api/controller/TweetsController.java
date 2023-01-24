f207d25815ad36ab6a3c0903a3da1665de446ce3
Thales Ruano <thalesoruano@gmail.com>
2023-01-23 23:54:39 -0300

    feat create /tweets GET rout

--- src/main/java/com/tweeteroo/api/controller/TweetsController.java
+++ src/main/java/com/tweeteroo/api/controller/TweetsController.java
@@ -1,18 +1,18 @@
 package com.tweeteroo.api.controller;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tweeteroo.api.dto.TweetDTO;
@@ -26,15 +26,49 @@ public class TweetsController {
     private List<Tweet> tweetList = new ArrayList<>();
 
     @GetMapping()
-    public ResponseEntity<HashMap<String, Object>> getTweets() {
+    public ResponseEntity<HashMap<String, Object>> getTweets(@RequestParam int page) {
 
-        HashMap<String, Object> tweetsMap = new HashMap<String, Object>();
-        tweetsMap.put("content", tweetList);
+        HashMap<String, Object> tweetsMap = new HashMap<>();
+
+        tweetsMap.put("content", makeAuxTweetList(tweetList, page));
 
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(tweetsMap);
 
+    }
+
+    public List<Tweet> makeAuxTweetList(List<Tweet> originalList, int page) {
+
+        final int totalTweets = originalList.size();
+        final int perPage = 5;
+
+        List<Tweet> auxList = new ArrayList<>();
+
+        int fromIndex = page * perPage;
+        Collections.reverse(originalList);
+
+        if (!originalList.isEmpty()) {
+            if (page == 0) {
+                if (totalTweets < perPage)
+                    auxList = originalList.subList(0, totalTweets);
+                else
+                    auxList = originalList.subList(0, perPage);
+
+            } else {
+                Collections.reverse(originalList);
+                int maxPageAllowed = totalTweets / perPage;
+
+                if (page < maxPageAllowed) {
+                    auxList = originalList.subList(fromIndex, (fromIndex + perPage));
+                } else if (page == maxPageAllowed) {
+                    int remaining = totalTweets % page;
+                    auxList = originalList.subList(fromIndex, (fromIndex + remaining));
+                } else
+                    auxList = new ArrayList<>();
+            }
+        }
+        return auxList;
     }
 
     @PostMapping()
@@ -48,7 +82,6 @@ public class TweetsController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body("OK");
-
     }
 
 }
